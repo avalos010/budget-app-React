f99389c572225bf3be5904c198f8041db35b5e1f
avalos010 <luizavalos40@gmail.com>
2017-10-08 18:07:09 -0500

    Updates

--- src/components/total.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Total = (props) => (
-	<div className="total"> 
-	<h2> TOTAL: ${props.income + props.expenses}</h2>
-	</div>
-);
-
-export default Total;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import App from './components/BudgetMain';
-
-ReactDOM.render(<App />, document.getElementById('root'));
