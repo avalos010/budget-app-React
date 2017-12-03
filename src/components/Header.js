9cd6f7ff088747aff128c4cae1ffcaffd1b3cb27
JLuis Avalos <luizavalos40@gmail.com>
2017-12-03 15:22:18 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (props) => (
 	<div className="header">
-		<h1> Budget App By <a href="www.joseluisdev.com" target="_blank">JLuis</a> </h1>
+		<h1> Budget App By <a href="http://www.joseluisdev.com" target="_blank">JLuis</a> </h1>
 	</div>
 );
 
